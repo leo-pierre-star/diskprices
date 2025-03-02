95ee5a44478d91e25dc2b51360deac0ffcc04fd1
Nicolas <nicolas.grissi@gmail.com>
2025-03-02 18:16:30 +0100

    feat: replace vuetify-nuxt-module by nuxtui

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="h-screen">
+    <Header />
+    <UContainer class="py-10">
+      <slot />
+    </UContainer>
+    <footer class="fixed bottom-0 w-full">
+      <UContainer>
+        <div class="flex justify-center items-center gap-2 w-full py-2">
+          <p class="text-xs text-gray-500">
+            Ce site est supporté par des liens affiliés.
+          </p>
+
+          <UButton
+            icon="i-heroicons:information-circle"
+            size="xs"
+            color="primary"
+            variant="outline"
+            label="En savoir plus"
+            to="/faq"
+            :trailing="false"
+          />
+        </div>
+      </UContainer>
+    </footer>
+  </div>
+</template>
+<script setup lang="ts"></script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,60 @@
 <template>
-    <v-container>
-      <v-row>
-        <v-col v-for="product in products" :key="product.id" cols="12" md="6">
-          <v-card>
-            <v-img :src="product.image" height="200px" cover></v-img>
-            <v-card-title>{{ product.title }}</v-card-title>
-            <v-card-actions>
-              <v-btn :href="product.link" target="_blank" color="primary">
-                Voir sur Amazon
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-      <p>Ce site est supporté par des liens affiliés.</p>
-      <v-btn to="/faq" color="primary" class="mb-4">
-        ℹ En savoir plus
-      </v-btn>
-    </v-container>
-  </template>
-  
+  <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-4">
+    <UCard
+      v-for="product in products"
+      :key="product.id"
+      class="overflow-hidden"
+      :ui="{
+        header: {
+          base: 'overflow-hidden',
+          background: '',
+          padding: 'px-0 py-0 sm:px-0',
+        },
+      }"
+    >
+      <template #header>
+        <img
+          class="object-cover aspect-video w-full"
+          :src="product.image"
+          :alt="product.title"
+        />
+      </template>
 
-  <script setup>
-  import products from '~/data/products';
+      <div class="flex flex-col gap-2">
+        <h3 class="line-clamp-3 text-sm text-gray-500">{{ product.title }}</h3>
+        <div class="text-pretty text-lg font-bold">
+          {{ formatPrice(product.price) }}€
+        </div>
+        <UButton
+          :ui="{ rounded: 'rounded-full' }"
+          block
+          icon="i-heroicons-pencil-square"
+          size="sm"
+          color="primary"
+          variant="outline"
+          label="Voir sur Amazon"
+          :to="product.link"
+          target="_blank"
+          :trailing="false"
+        />
+      </div>
+    </UCard>
+  </div>
+</template>
+
+<script setup>
+import products from "~/data/products";
+import { formatPrice } from "~/core/helpers/price";
+
+definePageMeta({
+  layout: "default",
+});
 </script>
+<style lang="scss" scoped>
+.line-clamp-3 {
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+</style>
